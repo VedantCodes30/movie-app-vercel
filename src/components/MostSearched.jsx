936573d93ab85f3c2fb7839d4d2606fcbd531f09
Vedant Kale <vedantcodes30@gmail.com>
2025-06-20 20:06:42 +0530

    'Fixed most searched and some SEO fixed'

--- src/components/MostSearched.jsx
+++ src/components/MostSearched.jsx
@@ -21,24 +21,26 @@ const MostSearched = () => {
       <h2 className="text-xl font-bold text-white mb-4 px-4">Most Searched</h2>
 
       <div className="overflow-x-auto scrollbar-thin scrollbar-thumb-purple-500 px-4 py-2">
-        <div className="flex gap-4 w-max">
+        <div className="flex gap-4 w-max ">
           {movies.map((movie) => (
-            <Link
-              to={`/movie/${movie.movie_id}`}
-              key={movie.$id}
-              className="w-[150px] sm:w-[180px] md:w-[200px] lg:w-[220px] flex-shrink-0 block rounded-lg overflow-hidden hover:scale-105 transition-transform shadow-md bg-[#1e0044]"
-            >
-              <img
-                src={movie.poster_url || "/no-movie.png"}
-                alt={movie.title || "Movie Poster"}
-                className="w-full h-60 object-cover"
-              />
-              <div>
-                <p className="text-sm font-bold truncate text-center text-white p-2">
-                  {movie.title || "Untitled"}
-                </p>
-              </div>
-            </Link>
+            <div className="movie-card" key={movie.$id}>
+              <Link
+                to={`/movie/${movie.movie_id}`}
+                key={movie.$id}
+                className="w-[150px] sm:w-[180px] md:w-[200px] lg:w-[220px] flex-shrink-0 block  hover:scale-105 transition-transform shadow-md "
+              >
+                <img
+                  src={movie.poster_url || "/no-movie.png"}
+                  alt={movie.title || "Movie Poster"}
+                  className="w-full min-h-[100%] object-cover"
+                />
+                <div className="content">
+                  <h3 className=" font-[600] truncate text-center text-white p-2">
+                    {movie.title || "Untitled"}
+                  </h3>
+                </div>
+              </Link>
+            </div>
           ))}
         </div>
       </div>
